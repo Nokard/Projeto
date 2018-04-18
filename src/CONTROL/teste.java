9f72e1427338059ccc420752a363f774fcd8dad6
Hugo <Hugo@DESKTOP-21CEKIQ>
2018-04-18 12:14:52 -0300

    classe teste

--- src/CONTROL/teste.java
+++ src/CONTROL/teste.java
@@ -2,4 +2,19 @@ package CONTROL;
 
 public class teste {
 
+	private String  Nome;
+	private String sexo;
+	
+	public String getNome() {
+		return Nome;
+	}
+	public void setNome(String nome) {
+		Nome = nome;
+	}
+	public String getSexo() {
+		return sexo;
+	}
+	public void setSexo(String sexo) {
+		this.sexo = sexo;
+	}
 }
