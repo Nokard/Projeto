8a49ce205add3c664d323ca99fe192982cd68cb4
Hugo <Hugo@DESKTOP-21CEKIQ>
2018-04-18 12:59:47 -0300

    add idade teste.java

--- src/CONTROL/teste.java
+++ src/CONTROL/teste.java
@@ -4,7 +4,15 @@ public class teste {
 
 	private String  Nome;
 	private String sexo;
+	private int idade;
 	
+	
+	public int getIdade() {
+		return idade;
+	}
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
 	public String getNome() {
 		return Nome;
 	}
