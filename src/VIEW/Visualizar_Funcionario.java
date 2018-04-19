00b4d75942dadbdf5174fec9014c8a072cbcca2f
Hugo <Hugo@DESKTOP-21CEKIQ>
2018-04-19 16:26:12 -0300

    visualizar_funcionario

---
+++ src/VIEW/Visualizar_Funcionario.java
@@ -0,0 +1,44 @@
+package VIEW;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import java.awt.ScrollPane;
+
+public class Visualizar_Funcionario extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Visualizar_Funcionario frame = new Visualizar_Funcionario();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Visualizar_Funcionario() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 596, 481);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+	}
+}
